00254f3cc4aa940d216aa6529c4341792fd53170
Aldycky1 <bagusaldycky@gmail.com>
2021-10-03 16:06:21 +0700

    setup bottom tab navigator

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input} from '../../components';
-import {colors} from '../../utils/colors';
+import {colors} from '../../utils';
 
 const Register = ({navigation}) => {
   return (
@@ -16,7 +16,10 @@ const Register = ({navigation}) => {
         <Gap height={24} />
         <Input label="Password" />
         <Gap height={24} />
-        <Button title="Continue" />
+        <Button
+          title="Continue"
+          onPress={() => navigation.navigate('UploadPhoto')}
+        />
       </View>
     </View>
   );
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {GetStarted, Login, Register, Splash} from '../pages';
+import {
+  GetStarted,
+  Login,
+  Register,
+  Splash,
+  UploadPhoto,
+  Doctor,
+  Messages,
+  Hospitals,
+} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Doctor" component={Doctor} />
+      <Tab.Screen name="Messages" component={Messages} />
+      <Tab.Screen name="Hospitals" component={Hospitals} />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -25,6 +46,16 @@ const Router = () => {
       <Stack.Screen
         name="Login"
         component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="UploadPhoto"
+        component={UploadPhoto}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
