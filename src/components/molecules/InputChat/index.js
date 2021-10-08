24197d5976fdf954bc44ddf1e8109decbd0fd917
Aldycky1 <bagusaldycky@gmail.com>
2021-10-08 09:25:49 +0700

    slicing chatting - slicing chatting item

--- src/components/molecules/InputChat/index.js
+++ src/components/molecules/InputChat/index.js
@@ -7,7 +7,7 @@ const InputChat = () => {
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} placeholder="Tulis pesan untuk Nairobi" />
-      <Button type="btn-icon-send" disable />
+      <Button type="btn-icon-send" />
     </View>
   );
 };
