d329b63e13d46a3a4ee2173fa698a1add83f4d0b
Aldycky1 <bagusaldycky@gmail.com>
2021-10-09 06:46:37 +0700

    slicing doctor profile screen

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {
+  DummyDoctor1,
+  DummyDoctor2,
+  DummyDoctor3,
+  JSONCategoryDoctor,
+} from '../../assets';
+import {
   DoctorCategory,
   Gap,
   HomeProfile,
@@ -8,7 +14,6 @@ import {
   RatedDoctor,
 } from '../../components';
 import {colors, fonts} from '../../utils';
-import {JSONCategoryDoctor} from '../../assets';
 
 const Doctor = ({navigation}) => {
   return (
@@ -41,9 +46,24 @@ const Doctor = ({navigation}) => {
           </View>
           <View style={styles.wrapperSection}>
             <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-            <RatedDoctor />
-            <RatedDoctor />
-            <RatedDoctor />
+            <RatedDoctor
+              name="Alexa Rachel"
+              desc="Pediatrician"
+              avatar={DummyDoctor1}
+              onPress={() => navigation.navigate('DoctorProfile')}
+            />
+            <RatedDoctor
+              name="Sunny Frank"
+              desc="Dentist"
+              avatar={DummyDoctor2}
+              onPress={() => navigation.navigate('DoctorProfile')}
+            />
+            <RatedDoctor
+              name="Poe Minn"
+              desc="Podiatrist"
+              avatar={DummyDoctor3}
+              onPress={() => navigation.navigate('DoctorProfile')}
+            />
             <Text style={styles.sectionLabel}>Good News</Text>
           </View>
           <NewsItem />
--- src/pages/DoctorProfile/index.js
+++ src/pages/DoctorProfile/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Button, Gap, Header, Profile, ProfileItem} from '../../components';
+import {colors} from '../../utils';
 
 const DoctorProfile = () => {
   return (
-    <View>
-      <Text>Doctor Profile Page</Text>
+    <View style={styles.page}>
+      <Header title="Doctor Profile" />
+      <Profile name="Nairobi Putri Hayza" desc="Dokter Anak" />
+      <Gap height={10} />
+      <ProfileItem label="Alumnus" value="Universitas Indonesia, 2015" />
+      <ProfileItem label="Tempat Praktik" value="Rumah Sakit Umum, Bandung" />
+      <ProfileItem label="No. STR" value="0000116622081996" />
+      <View style={styles.action}>
+        <Button title="Start Consultation" />
+      </View>
     </View>
   );
 };
 
 export default DoctorProfile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {backgroundColor: colors.white, flex: 1},
+  action: {paddingHorizontal: 40, paddingTop: 23},
+});
