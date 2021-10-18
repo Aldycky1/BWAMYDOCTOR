f5d5ea65afb477bcfbbeb7870dd1d630edf36d30
Aldycky1 <bagusaldycky@gmail.com>
2021-10-18 16:47:00 +0700

    implementasi redux untuk memunculkan loading secara global

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useState} from 'react';
+import React from 'react';
 import FlashMessage from 'react-native-flash-message';
-import {Provider} from 'react-redux';
+import {Provider, useSelector} from 'react-redux';
 import {Loading} from './components';
 import store from './redux/store';
 import Router from './router';
 
-const App = () => {
-  const [loading, setLoading] = useState(false);
+const MainApp = () => {
+  const stateGlobal = useSelector(state => state);
+  console.log('state global: ', stateGlobal);
   return (
-    <Provider store={store}>
+    <>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-      {loading && <Loading />}
+      {stateGlobal.loading && <Loading />}
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <MainApp />
     </Provider>
   );
 };
